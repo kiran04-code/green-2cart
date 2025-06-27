54bdf8fb3d649aed5714c51926bdb390ca096631
kiran04-code <kr551344@gmail.com>
2025-06-28 00:08:12 +0530

    alldonelastCommit5

--- server/controller/user.js
+++ server/controller/user.js
@@ -2,51 +2,51 @@ import user from "../model/user.js";
 import bcrypt from "bcrypt";
 import { createToken } from "../auth/jwt.js";
 export const hnadleUserSignup = async (req, res) => {
-    const { firstName, email, password, } = req.body
-    if(!email|| !password || !firstName){
-         return res.json({
-            success:false,
-            message:"missing Details"
-        })
-    }
-    const findUser = await user.findOne({ email })
-    if (findUser) {
-     return   res.json(({
-            message: "user Already Exist",
-            success: false
-        }))
+  const { firstName, email, password, } = req.body
+  if (!email || !password || !firstName) {
+    return res.json({
+      success: false,
+      message: "missing Details"
+    })
+  }
+  const findUser = await user.findOne({ email })
+  if (findUser) {
+    return res.json(({
+      message: "user Already Exist",
+      success: false
+    }))
 
-    }
-    else {
-        const haspassword = await bcrypt.hash(password, 10)
-        const createdUser = await user.create({
-            email,
-            password: haspassword,
-            firstName
-        })
+  }
+  else {
+    const haspassword = await bcrypt.hash(password, 10)
+    const createdUser = await user.create({
+      email,
+      password: haspassword,
+      firstName
+    })
 
-        const token = createToken(createdUser)
-      return res.cookie("token_user_login", token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==="production", // user secure when it production,
-        sameSite:process.env.NODE_ENV==="production"?"none":"strict", //CSRF PRODUCTION,
-        maxAage:7*24*60*60*1000
+    const token = createToken(createdUser)
+    return res.cookie("token_user_login", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // HTTPS only
+      sameSite: 'none', // or 'none' if cross-site
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
 
-      }).json({
-            success: true,
-            message: "Account successfully created!",
-            userData: createdUser
-        })
-    }
+    }).json({
+      success: true,
+      message: "Account successfully created!",
+      userData: createdUser
+    })
+  }
 }
 export const handleLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
-      if(!email || !password){
-         return res.json({
-            success:false,
-            message:"missing Details"
-        })
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        message: "missing Details"
+      })
     }
     // Find user by email
     const finduser = await user.findOne({ email });
@@ -77,12 +77,12 @@ export const handleLogin = async (req, res) => {
     }).json({
       success: true,
       message: "Login successful!",
-      userData:finduser
+      userData: finduser
     });
 
   } catch (error) {
     console.error("Login error:", error);
-   return res.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "Server error during login"
     });
@@ -90,83 +90,89 @@ export const handleLogin = async (req, res) => {
 };
 
 export const handleLoginWithGoogle = async (req, res) => {
-    const { email, firtstName  } = req.body;
-    try {
-        const existingUser = await user.findOne({ email });
+  const { email, firtstName } = req.body;
+  try {
+    const existingUser = await user.findOne({ email });
 
-        if (existingUser) {
-            const token = createToken(existingUser);
-            return res
-                .cookie("token_user_login", token, { httpOnly: true, secure: true })
-                .json({
-                    message: "Login successful!",
-                    success: true,
-                    userData: existingUser
-                });
-        } else {
-            const randomPassword = Math.floor(100000 + Math.random() * 900000).toString();
-            const hashedPassword = await bcrypt.hash(randomPassword, 10);
+    if (existingUser) {
+      const token = createToken(existingUser);
+      return res
+        .cookie("token_user_login", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production', // HTTPS only
+          sameSite: 'none', // or 'none' if cross-site
+          maxAge: 24 * 60 * 60 * 1000 // 1 day
+        })
+        .json({
+          message: "Login successful!",
+          success: true,
+          userData: existingUser
+        });
+    } else {
+      const randomPassword = Math.floor(100000 + Math.random() * 900000).toString();
+      const hashedPassword = await bcrypt.hash(randomPassword, 10);
 
-            const newUser = await user.create({
-                email,
-                firstName:firtstName,
-                password: hashedPassword
-            });
+      const newUser = await user.create({
+        email,
+        firstName: firtstName,
+        password: hashedPassword
+      });
 
-            const token = createToken(newUser);
-            return res
-                .cookie("token_user_login", token, { httpOnly: true, secure: true })
-                .json({
-                    message: "Account created and login successful!",
-                    success: true,
-                    userData: newUser
-                });
-        }
-    } catch (error) {
-        console.error("Google login error:", error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server Error",
-            error: error.message
+      const token = createToken(newUser);
+      return res
+        .cookie("token_user_login", token, { httpOnly: true, secure: true })
+        .json({
+          message: "Account created and login successful!",
+          success: true,
+          userData: newUser
         });
     }
+  } catch (error) {
+    console.error("Google login error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message
+    });
+  }
 };
-export const isAuth = async(req,res)=>{
+export const isAuth = async (req, res) => {
   try {
     const userid = req.user
     const users = await user.findById(userid).select("-password")
-    res.json({success:true,userData:users})
+    res.json({ success: true, userData: users })
   } catch (error) {
     console.log(error.message)
-    res.json({success:false,message:error.message})
+    res.json({ success: false, message: error.message })
   }
 }
-export const authcheck = async ()=>{
- try {
-   if(req.user){
-    const findUser = await user.findById(req.user)
-    res.json({
-      success:true,
-      message:"user is Authorized "
-    })
+export const authcheck = async () => {
+  try {
+    if (req.user) {
+      const findUser = await user.findById(req.user)
+      res.json({
+        success: true,
+        message: "user is Authorized "
+      })
+    }
+  } catch (error) {
+    console.log(error)
   }
- } catch (error) {
-  console.log(error)
- }
 }
 export const logout = async (req, res) => {
- try {
-   res.clearCookie("token_user_login", {
-    httpOnly: true,
-    secure: process.NODE_ENV ==="production"   ,    // only enable in production or HTTPS
-    sameSite: process.NODE_ENV ==="production" ?"none" :"strict"    // optional but adds CSRF protection
-  });
+  try {
+    res.clearCookie("token_user_login", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // HTTPS only
+      sameSite: 'none', // or 'none' if cross-site
+      maxAge: 24 * 60 * 60 * 1000 // 1 dayn
+    });
 
-  return res.json({
-    success: true,
-    message: "Logout successful!"
-  });
- } catch (error) {
-  console.log(error)
- }
+    return res.json({
+      success: true,
+      message: "Logout successful!"
+    });
+  } catch (error) {
+    console.log(error)
+  }
 };
